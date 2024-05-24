56167edd4a557679cf2b94c5c3cd1fdc8518b386
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-05-24 20:27:56 +0700

    Fix circular dependencies

--- src/Draw/GraphicObject/Line.cpp
+++ src/Draw/GraphicObject/Line.cpp
@@ -5,4 +5,62 @@
 #include "../Line.h"
 
 namespace graphic {
+
+    inline Screen& Line::displayOn(Screen& screen) const {
+        int dx = std::abs(x1 - x0), dy = std::abs(y1 - y0);
+        int sx = x0 < x1 ? 1 : -1, sy = y0 < y1 ? 1 : -1;
+        int error = dx + dy;
+
+        int t_x0 = x0, t_y0 = y0, t_x1 = x1, t_y1 = y1;
+        while (true) {
+            screen.changeAt(255, 255, 255, t_x0, t_y0);
+
+            if (t_x0 == t_x1 && t_y0 == t_y1) break;
+
+            int e2 = 2 * error;
+
+            if (e2 >= dy) {
+                if (t_x0 == t_x1) break;
+                error = error + dy;
+                t_x0 += sx;
+            }
+
+            if (e2 <= dx) {
+                if (t_y0 == t_y1) break;
+                error = error + dx;
+                t_y0 += sy;
+            }
+        }
+
+        return screen;
+    }
+
+    Line::Line(int x0, int y0, int x1, int y1) : x0(x0), y0(y0), x1(x1), y1(y1) {}
+
+    Screen& operator<<(Screen& screen, const Line*& line) {
+        if (line == nullptr) return screen;
+        return line->displayOn(screen);
+    }
+
+    Screen& operator<<(Screen& screen, const Line& line) {
+        return line.displayOn(screen);
+    }
+
+    Screen& operator<<(Screen* screen, const Line& line) {
+        return line.displayOn(*screen);
+    }
+
+    Screen& Line::operator>>(Screen& screen) const {
+        return displayOn(screen);
+    }
+
+
+    Screen& Line::operator>>(Screen* screen) const {
+        return displayOn(*screen);
+    }
+
+    Screen& Line::operator>>(Screen*& screen) const {
+        return displayOn(*screen);
+    }
+
 } // graphic
--- src/Draw/Line.h
+++ src/Draw/Line.h
@@ -14,97 +14,28 @@ namespace graphic {
         int x0, y0, x1, y1;
 
     private:
-        Screen& displayOn(Screen& screen) const override {
-            /*
-            dx = abs(x1 - x0)
-            dy = -abs(y1 - y0)
-
-            sx = x0 < x1 ? 1 : -1
-            sy = y0 < y1 ? 1 : -1
-
-            error = dx + dy
-
-            while true {
-                plot(x0, y0)
-
-                if x0 == x1 && y0 == y1
-                    break
-
-                e2 = 2 * error
-                if e2 >= dy {
-                    if x0 == x1 break
-                    error = error + dy
-                    x0 = x0 + sx
-                }
-
-                if e2 <= dx {
-                    if y0 == y1
-                        break
-
-                    error = error + dx
-                    y0 = y0 + sy
-                }
-            }
-             */
-            int dx = std::abs(x1 - x0), dy = std::abs(y1 - y0);
-            int sx = x0 < x1 ? 1 : -1, sy = y0 < y1 ? 1 : -1;
-            int error = dx + dy;
-
-            int t_x0 = x0, t_y0 = y0, t_x1 = x1, t_y1 = y1;
-            while (true) {
-                screen.changeAt(255, 255, 255, t_x0, t_y0);
-
-                if (t_x0 == t_x1 && t_y0 == t_y1) break;
-
-                int e2 = 2 * error;
-
-                if (e2 >= dy) {
-                    if (t_x0 == t_x1) break;
-                    error = error + dy;
-                    t_x0 += sx;
-                }
-
-                if (e2 <= dx) {
-                    if (t_y0 == t_y1) break;
-                    error = error + dx;
-                    t_y0 += sy;
-                }
-            }
-
-            return screen;
-        }
+        inline Screen& displayOn(Screen& screen) const override;
 
     public:
-        Line(int x0, int y0, int x1, int y1) : x0(x0), y0(y0), x1(x1), y1(y1) {}
+        Line(int x0, int y0, int x1, int y1);
 
-        friend Screen& operator<<(Screen& screen, const Line*& line) {
-            if (line == nullptr) return screen;
-            return line->displayOn(screen);
-        }
+        friend Screen& operator<<(Screen& screen, const Line*& line);
 
-        friend Screen& operator<<(Screen& screen, const Line& line) {
-            return line.displayOn(screen);
-        }
+        friend Screen& operator<<(Screen& screen, const Line& line);
 
-        friend Screen& operator<<(Screen* screen, const Line& line) {
-            return line.displayOn(*screen);
-        }
+        friend Screen& operator<<(Screen* screen, const Line& line);
 
-        Screen& operator>>(Screen& screen) const override {
-            return displayOn(screen);
-        }
+        Screen& operator>>(Screen& screen) const override;
 
-        Screen& operator>>(Screen* screen) const override {
-            return displayOn(*screen);
-        }
+        Screen& operator>>(Screen* screen) const override;
 
-        Screen& operator>>(Screen*& screen) const override {
-            return displayOn(*screen);
-        }
+        Screen& operator>>(Screen*& screen) const override;
 
         ~Line() = default;
     };
 
 } // graphic
+
+#include "../Screen.h"
 
 #endif //COMPUTERGRAPHIC_LINE_H
