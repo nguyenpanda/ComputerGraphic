87061f278764653bf40afd59ace3514c72393dcb
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-05-27 18:15:33 +0700

    Add Utility.h

---
+++ src/Utility.h
@@ -0,0 +1,12 @@
+//
+// Created by Hà Tường Nguyên on 5/27/24.
+//
+
+#ifndef COMPUTERGRAPHIC_UTILITY_H
+#define COMPUTERGRAPHIC_UTILITY_H
+
+#include "Utility/color.h"
+#include "Utility/performance.h"
+#include "Utility/PriorityQueue.h"
+
+#endif //COMPUTERGRAPHIC_UTILITY_H
