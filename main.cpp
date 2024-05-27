a066b5dedb8546d7cc9dfa6ea410c3a7d0c34cd3
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-05-27 18:39:21 +0700

    Run all testcase on main

--- main.cpp
+++ main.cpp
@@ -3,17 +3,16 @@
 int main() {
     std::cout << color::GREEN << "Start program" << color::RESET << std::endl;
 
-//    testcase::change_at0();
-//    testcase::change_at1();
-//    testcase::change_at2(100, 100);
+    testcase::change_at0();
+    testcase::change_at1();
+    testcase::change_at2(100, 100);
 
-//    testcase::draw0();
-//    testcase::draw1(33, 42, -16, 33);
+    testcase::draw0();
+    testcase::draw1(33, 42, -16, 33);
     testcase::draw2();
 
-//    testcase::plot0();
-//    testcase::plot1(72, 90, -33, 33);
-
+    testcase::plot0();
+    testcase::plot1(72, 90, -33, 33);
 
     std::cout << color::GREEN << "End program" << color::RESET << std::endl;
     return 0;
