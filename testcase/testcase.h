0f5f8bac0e8042aad890e32795d1b28176d9ec0d
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-05-28 11:52:18 +0700

    Split header file

--- testcase/testcase.h
+++ testcase/testcase.h
@@ -5,26 +5,14 @@
 #ifndef COMPUTERGRAPHIC_TESTCASE_H
 #define COMPUTERGRAPHIC_TESTCASE_H
 
+#include "Screen/test_changeAt.h"
+#include "Screen/test_operator.h"
+#include "Screen/test_plot.h"
+#include "Object/test_Line.h"
+
 #include "../src/GraphicScreen.h"
 
-namespace testcase {
-
-    void change_at0(int w = 28, int h = 28);
-
-    void change_at1(int w = 28, int h = 28);
-
-    void change_at2(int w = 28, int h = 28);
-
-    void draw0(int w = 28, int h = 28);
-
-    void draw1(int w = 100, int h = 100, int start = 0, size_t N = 100);
-
-    void draw2(int w = 50, int h = 50);
-
-    void plot0(int w = 33, int h = 42, int x_start = -16, int x_end = 16);
-
-    void plot1(int w = 50, int h = 36, int x_start = -24, int x_end = 24);
-
-}
+#include <cmath>
+#include <cstdint>
 
 #endif //COMPUTERGRAPHIC_TESTCASE_H
