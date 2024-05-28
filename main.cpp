14b328050587d53a15cdac2bc6f0f0d140148a63
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-05-28 11:49:15 +0700

    Add testcase for << file writing operator

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int main() {
+int main(int argc, char* argv[]) {
     std::cout << color::GREEN << "Start program" << color::RESET << std::endl;
 
     testcase::change_at0();
@@ -13,6 +13,11 @@ int main() {
 
     testcase::plot0();
     testcase::plot1(72, 90, -33, 33);
+    testcase::plot2();
+    testcase::plot3();
+
+    testcase::write_ofstream0();
+    testcase::write_fstream0();
 
     std::cout << color::GREEN << "End program" << color::RESET << std::endl;
     return 0;
