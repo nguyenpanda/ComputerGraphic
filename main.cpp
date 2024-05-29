164840afbf29cd350d410f36c25fff1a7b950f01
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-05-29 23:07:45 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,10 +1,9 @@
 #include "main.h"
-#include "sub_code/Dataset.h"
 
 int main(int argc, char* argv[]) {
     std::cout << color::GREEN << "Start program" << color::RESET << std::endl;
 
-    if (argc == 0) {
+    if (argc == 1) {
         testcase::all();
         return 0;
     }
@@ -14,7 +13,7 @@ int main(int argc, char* argv[]) {
     for (int i = 1; i < argc; ++i) {
         if (std::strcmp(argv[i], "-f") == 0) {
             if (i + 1 < argc) {
-                test_animation(argv[++i]);
+                AI::test_animation(argv[++i]);
             } else {
                 std::cout << color::RED << "Error: -f option requires an argument." << std::endl;
                 return 1;
