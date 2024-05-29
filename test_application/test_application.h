75eb32c9c683bd56ff7e9ff8f449152c942c5537
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-05-29 23:06:49 +0700

    First commit

---
+++ test_application/test_application.h
@@ -0,0 +1,10 @@
+//
+// Created by Hà Tường Nguyên on 5/29/24.
+//
+
+#ifndef COMPUTERGRAPHIC_TEST_APPLICATION_H
+#define COMPUTERGRAPHIC_TEST_APPLICATION_H
+
+#include "digit_recognizer/Dataset.h"
+
+#endif //COMPUTERGRAPHIC_TEST_APPLICATION_H
