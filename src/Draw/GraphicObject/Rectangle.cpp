d6706cefde83e7db501deb5f8d43a9b8d0a11944
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-05-30 17:06:21 +0700

    Add graphic::Rectangle class

---
+++ src/Draw/GraphicObject/Rectangle.cpp
@@ -0,0 +1,39 @@
+//
+// Created by Hà Tường Nguyên on 5/28/24.
+//
+
+#include "Rectangle.h"
+
+namespace graphic {
+
+    inline Screen& Rectangle::displayOn(Screen& screen) const {
+        int m = y + h - 1;
+        int n = x + w - 1;
+        for (int i = 0; i < w; ++i) {
+            screen.changeAt(255, 255, 255, x + i, y);
+            screen.changeAt(255, 255, 255, x + i, m);
+        }
+
+        for (int i = 1; i < h - 1; ++i) {
+            screen.changeAt(255, 255, 255, x, y + i);
+            screen.changeAt(255, 255, 255, n, y + i);
+        }
+        return screen;
+    }
+
+    Rectangle::Rectangle(int x, int y, int w, int h) : x(x), y(y), w(w), h(h) {}
+
+    Screen& operator<<(Screen& screen, const Rectangle*& rectangle) {
+        if (rectangle == nullptr) return screen;
+        return rectangle->displayOn(screen);
+    }
+
+    Screen& operator<<(Screen& screen, const Rectangle& rectangle) {
+        return rectangle.displayOn(screen);
+    }
+
+    Screen& operator<<(Screen* screen, const Rectangle& rectangle) {
+        return rectangle.displayOn(*screen);
+    }
+
+} // graphic
---
+++ src/Draw/GraphicObject/Rectangle.h
@@ -0,0 +1,36 @@
+//
+// Created by Hà Tường Nguyên on 5/28/24.
+//
+
+#ifndef COMPUTERGRAPHIC_RECTANGLE_H
+#define COMPUTERGRAPHIC_RECTANGLE_H
+
+#include "Object.h"
+
+namespace graphic {
+
+    class Rectangle : public Object {
+    private:
+        int x, y, w, h;
+
+    private:
+        inline Screen& displayOn(Screen& screen) const override;
+
+    public:
+        Rectangle(int x, int y, int w, int h);
+
+        friend Screen& operator<<(Screen& screen, const Rectangle*& rectangle);
+
+        friend Screen& operator<<(Screen& screen, const Rectangle& rectangle);
+
+        friend Screen& operator<<(Screen* screen, const Rectangle& rectangle);
+
+        ~Rectangle() = default;
+
+    };
+
+}
+
+#include "../../PixelScreen/Screen.h"
+
+#endif //COMPUTERGRAPHIC_RECTANGLE_H
