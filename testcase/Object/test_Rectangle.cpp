771c47856791bfb2dbef12d7f4cc776ef731e042
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-05-30 17:07:10 +0700

    Add testcase for graphic::Rectangle class

---
+++ testcase/Object/test_Rectangle.cpp
@@ -0,0 +1,50 @@
+//
+// Created by Hà Tường Nguyên on 5/30/24.
+//
+
+#include "test_Rectangle.h"
+
+namespace testcase {
+
+    void rectangle0(int w, int h) {
+        std::cout << color::YELLOW << "testcase::rectangle 0" << color::RESET << std::endl;
+
+        graphic::Screen src(w, h);
+
+        src << graphic::Rectangle(0, 0, 6, 6);
+        src << graphic::Rectangle(1, 1, 5, 5);
+        src << graphic::Rectangle(2, 2, 4, 4);
+        src << graphic::Rectangle(0, 0, w, h);
+        std::cout << "------------------" << std::endl;
+        std::cout << src;
+
+        src.to_text("rectangle0.txt");
+    }
+
+    void rectangle1(int w, int h) {
+        std::cout << color::YELLOW << "testcase::rectangle 1" << color::RESET << std::endl;
+        graphic::Screen src(w, h);
+
+        src.drawrectangle(3, 5, 3, 3);
+        src.drawrectangle(8, 1, 7, 5);
+        src.drawrectangle(8, 6, 8, 3);
+        std::cout << "------------------" << std::endl;
+        std::cout << src;
+
+        src.to_text("rectangle1.txt");
+    }
+
+    void rectangle2(int x, int y, int w, int h) {
+        std::cout << color::YELLOW << "testcase::rectangle 2" << color::RESET << std::endl;
+        graphic::Screen src(w, h);
+
+        for (int i = 0; i < 6; ++i) {
+            src.drawrectangle(x + i, y + i, 6 - i, 6 - i);
+        }
+        std::cout << "------------------" << std::endl;
+        std::cout << src;
+
+        src.to_text("rectangle2.txt");
+    }
+
+}
---
+++ testcase/Object/test_Rectangle.h
@@ -0,0 +1,20 @@
+//
+// Created by Hà Tường Nguyên on 5/30/24.
+//
+
+#ifndef COMPUTERGRAPHIC_TEST_RECTANGLE_H
+#define COMPUTERGRAPHIC_TEST_RECTANGLE_H
+
+#include "../testcase.h"
+
+namespace testcase {
+
+    void rectangle0(int w = 10, int h = 10);
+
+    void rectangle1(int w = 20, int h = 20);
+
+    void rectangle2(int x = 1, int y = 1, int w = 10, int h = 10);
+
+}
+
+#endif //COMPUTERGRAPHIC_TEST_RECTANGLE_H
