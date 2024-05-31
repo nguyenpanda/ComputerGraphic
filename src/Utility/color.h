f9efae36a391be5749f360bf3368d2aa4ea7e979
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-05-31 22:45:27 +0700

    Split func definition and declaration

--- src/Utility/color.h
+++ src/Utility/color.h
@@ -29,36 +29,23 @@ public:
 
     class fourBit {
     public:
-        static std::string get(uint8_t SGR, int color) {
-            // SGR: Select Graphic Rendition
-            return "\033[" + std::to_string(SGR) + ";" + std::to_string(color) + "m";
-        }
+        static std::string get(uint8_t SGR, int color);
 
-        static std::string foreground(int color) {
-            return "\033[1;" + std::to_string(color) + "m";
-        }
+        static std::string foreground(int color);
     };
 
     class eightBit {
     public:
-        static std::string foreground(uint8_t color) {
-            return "\033[38;5;" + std::to_string(color) + "m";
-        }
+        static std::string foreground(uint8_t color);
 
-        static std::string background(uint8_t color) {
-            return "\033[48;5;" + std::to_string(color) + "m";
-        }
+        static std::string background(uint8_t color);
     };
 
     class two4Bit {
     public:
-        static std::string foreground(uint8_t r, uint8_t g, uint8_t b) {
-            return "\033[38;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m";
-        }
+        static std::string foreground(uint8_t r, uint8_t g, uint8_t b);
 
-        static std::string background(uint8_t r, uint8_t g, uint8_t b) {
-            return "\033[48;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m";
-        }
+        static std::string background(uint8_t r, uint8_t g, uint8_t b);
     };
 };
 
