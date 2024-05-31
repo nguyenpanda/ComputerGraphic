8cf0a56d7bd63a3a1d1a4fb703747dc327256530
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-05-31 17:54:43 +0700

    First commit

---
+++ custom_test.h
@@ -0,0 +1,30 @@
+//
+// Created by Hà Tường Nguyên on 5/31/24.
+//
+
+#ifndef COMPUTERGRAPHIC_CUSTOM_TEST_H
+#define COMPUTERGRAPHIC_CUSTOM_TEST_H
+
+#include "src/GraphicScreen.h"
+#include "src/Utility.h"
+
+#include <chrono>
+#include <thread>
+#include <unistd.h>
+#include <cstring>
+#include <algorithm>
+#include <functional>
+#include <random>
+#include <typeinfo>
+
+namespace cmdline {
+
+    void custom_func() {
+
+        // ADD YOUR CODE HERE
+
+    }
+
+}
+
+#endif //COMPUTERGRAPHIC_CUSTOM_TEST_H
