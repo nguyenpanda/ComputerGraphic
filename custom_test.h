0f04e6127c7290bff157c2b249feb28cf251a837
RestingWiki <126898951+RestingWiki@users.noreply.github.com>
2024-06-02 00:50:29 +0700

    Test copy constructor for Screen

--- custom_test.h
+++ custom_test.h
@@ -17,11 +17,23 @@
 #include <random>
 #include <typeinfo>
 
+using std::cout;
+using std::endl;
 namespace cmdline {
 
     void custom_func() {
 
         // ADD YOUR CODE HERE
+        graphic::Screen src(28,28);
+        graphic::Screen temp_src = src;
+        temp_src.drawrectangle(5,5,10,12);
+        //graphic::Screen temp_src2(28,28);
+        //temp_src.drawrectangle(5,5,10,10);
+        //temp_src2 = src;
+        //src.drawrectangle(5,5,10,10);
+        src.drawrectangle(0,0,5,5);
+        cout << src;
+
 
     }
 
