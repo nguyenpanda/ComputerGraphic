362b7274d3a20ebd5d0248f2368ef88f3f3db59e
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-06-04 03:27:48 +0700

    First commit

---
+++ testcase/Image/test_Bitmap.h
@@ -0,0 +1,22 @@
+//
+// Created by Hà Tường Nguyên on 6/4/24.
+//
+
+#ifndef COMPUTERGRAPHIC_TEST_BITMAP_H
+#define COMPUTERGRAPHIC_TEST_BITMAP_H
+
+#include "../testcase.h"
+
+namespace testcase {
+
+    void all_bitmap();
+
+    void bitmap0();
+
+    void bitmap1();
+
+    void bitmap2();
+
+}
+
+#endif //COMPUTERGRAPHIC_TEST_BITMAP_H
