a1ce6faccf23991c3c4f8cabc575ece4d31f45ba
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-06-06 00:50:29 +0700

    Add attribute alpha channel

--- src/PixelScreen/Pixel.cpp
+++ src/PixelScreen/Pixel.cpp
@@ -8,31 +8,51 @@ namespace graphic {
 
     Pixel::Pixel() {
         r = g = b = 0;
+        a = 255;
     }
 
     Pixel::Pixel(int color) {
         if (color > 255) color = 255;
         r = color, g = color, b = color;
+        a = 255;
     }
 
     Pixel::Pixel(int _r, int _g, int _b) {
         r = (_r > 255) ? 255 : _r;
         g = (_g > 255) ? 255 : _g;
         b = (_b > 255) ? 255 : _b;
+        a = 255;
+    }
+
+    Pixel::Pixel(int _r, int _g, int _b, int _a) {
+        r = (_r > 255) ? 255 : _r;
+        g = (_g > 255) ? 255 : _g;
+        b = (_b > 255) ? 255 : _b;
+        a = (_a > 255) ? 255 : _a;
     }
 
     Pixel& Pixel::operator=(const Pixel& other) {
         if (this == &other) return *this;
-
         r = other.r;
         g = other.g;
         b = other.b;
-
+        a = other.a;
         return *this;
     }
 
     void Pixel::set(int _r, int _g, int _b) {
         r = _r, g = _g, b = _b;
+    }
+
+    void Pixel::set(int _r, int _g, int _b, int _a) {
+        r = (_r > 255) ? 255 : _r;
+        g = (_g > 255) ? 255 : _g;
+        b = (_b > 255) ? 255 : _b;
+        a = (_a > 255) ? 255 : _a;
+    }
+
+    void Pixel::set_alpha(int _a) {
+        a = _a;
     }
 
     void Pixel::set(int gray_value) {
@@ -55,36 +75,40 @@ namespace graphic {
         return b;
     }
 
+    uint8_t Pixel::A() const {
+        return a;
+    }
+
     int Pixel::gray() const {
         return (int) (0.3 * r + 0.59 * g + 0.11 * b);
     }
 
-    Pixel Pixel::red() {
-        return Pixel(255, 0, 0);
+    Pixel Pixel::red(int _a) {
+        return Pixel(255, 0, 0, _a);
     }
 
-    Pixel Pixel::green() {
-        return Pixel(0, 255, 0);
+    Pixel Pixel::green(int _a) {
+        return Pixel(0, 255, 0, _a);
     }
 
-    Pixel Pixel::yellow() {
-        return Pixel(255, 255, 0);
+    Pixel Pixel::yellow(int _a) {
+        return Pixel(255, 255, 0, _a);
     }
 
-    Pixel Pixel::blue() {
-        return Pixel(0, 0, 255);
+    Pixel Pixel::blue(int _a) {
+        return Pixel(0, 0, 255, _a);
     }
 
-    Pixel Pixel::magenta() {
-        return Pixel(255, 0, 255);
+    Pixel Pixel::magenta(int _a) {
+        return Pixel(255, 0, 255, _a);
     }
 
-    Pixel Pixel::cyan() {
-        return Pixel(0, 255, 255);
+    Pixel Pixel::cyan(int _a) {
+        return Pixel(0, 255, 255, _a);
     }
 
-    Pixel Pixel::white() {
-        return Pixel(255, 255, 255);
+    Pixel Pixel::white(int _a) {
+        return Pixel(255, 255, 255, _a);
     }
 
     std::ostream& operator<<(std::ostream& _cout, const Pixel& pixel) {
--- src/PixelScreen/Pixel.h
+++ src/PixelScreen/Pixel.h
@@ -20,7 +20,7 @@ namespace graphic {
         friend std::ostream& operator<<(std::ostream& _cout, const Pixel& pixel);
 
     private:
-        uint8_t r, g, b;
+        uint8_t r, g, b, a;
 
     public:
         [[maybe_unused]] Pixel();
@@ -29,11 +29,17 @@ namespace graphic {
 
         [[maybe_unused]] explicit Pixel(int _r, int _g, int _b);
 
+        [[maybe_unused]] explicit Pixel(int _r, int _g, int _b, int _a);
+
         ~Pixel() = default;
 
         Pixel& operator=(const Pixel& other);
 
         void set(int _r, int _g, int _b);
+
+        void set(int _r, int _g, int _b, int _a);
+
+        void set_alpha(int _a);
 
         void set(int gray_value);
 
@@ -45,21 +51,23 @@ namespace graphic {
 
         [[nodiscard]] [[maybe_unused]] uint8_t B() const;
 
+        [[nodiscard]] [[maybe_unused]] uint8_t A() const;
+
         [[nodiscard]] [[maybe_unused]] int gray() const;
 
-        [[maybe_unused]] static Pixel red();
+        [[maybe_unused]] static Pixel red(int _a = 255);
 
-        [[maybe_unused]] static Pixel green();
+        [[maybe_unused]] static Pixel green(int _a = 255);
 
-        [[maybe_unused]] static Pixel yellow();
+        [[maybe_unused]] static Pixel yellow(int _a = 255);
 
-        [[maybe_unused]] static Pixel blue();
+        [[maybe_unused]] static Pixel blue(int _a = 255);
 
-        [[maybe_unused]] static Pixel magenta();
+        [[maybe_unused]] static Pixel magenta(int _a = 255);
 
-        [[maybe_unused]] static Pixel cyan();
+        [[maybe_unused]] static Pixel cyan(int _a = 255);
 
-        [[maybe_unused]] static Pixel white();
+        [[maybe_unused]] static Pixel white(int _a = 255);
 
         struct Compare {
             struct Less {
