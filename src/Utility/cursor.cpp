419569504cca7a8dc430b6205426c0b24d22f2e9
“Hatuongnguyen” <hatuongnguyen0107@gmail.com>
2024-06-06 15:10:02 +0700

    Add public tag for cursor class =))

--- src/Utility/cursor.cpp
+++ src/Utility/cursor.cpp
@@ -5,49 +5,49 @@
 #include "cursor.h"
 
 std::string cursor::up(int n) {
-    return "\033" + std::to_string(n) + "A";
+    return "\033[" + std::to_string(n) + "A";
 }
 
 std::string cursor::down(int n) {
-    return "\033" + std::to_string(n) + "B";
+    return "\033[" + std::to_string(n) + "B";
 }
 
 std::string cursor::forward(int n) {
-    return "\033" + std::to_string(n) + "C";
+    return "\033[" + std::to_string(n) + "C";
 }
 
 std::string cursor::back(int n) {
-    return "\033" + std::to_string(n) + "D";
+    return "\033[" + std::to_string(n) + "D";
 }
 
 std::string cursor::nextline(int n) {
-    return "\033" + std::to_string(n) + "E";
+    return "\033[" + std::to_string(n) + "E";
 }
 
 std::string cursor::preline(int n) {
-    return "\033" + std::to_string(n) + "F";
+    return "\033[" + std::to_string(n) + "F";
 }
 
 std::string cursor::horizonAbs(int n) {
-    return "\033" + std::to_string(n) + "G";
+    return "\033[" + std::to_string(n) + "G";
 }
 
 std::string cursor::position(int r, int c) {
-    return "\033" + std::to_string(r) + ";" + std::to_string(c) + "H";
+    return "\033[" + std::to_string(r) + ";" + std::to_string(c) + "H";
 }
 
 std::string cursor::eraseDis(int n) {
-    return "\033" + std::to_string(n) + "J";
+    return "\033[" + std::to_string(n) + "J";
 }
 
 std::string cursor::eraseLine(int n) {
-    return "\033" + std::to_string(n) + "K";
+    return "\033[" + std::to_string(n) + "K";
 }
 
 std::string cursor::scrollUp(int n) {
-    return "\033" + std::to_string(n) + "S";
+    return "\033[" + std::to_string(n) + "S";
 }
 
 std::string cursor::scrollDown(int n) {
-    return "\033" + std::to_string(n) + "T";
+    return "\033[" + std::to_string(n) + "T";
 }
